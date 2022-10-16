8d35470f5f55c6e3d7a55584b143a0ca1fbfe1c2
linguanghan <1354009873@qq.com>
2022-10-16 15:17:36 +0800

    fix: 去除多余代码

--- pic_word_create.py
+++ pic_word_create.py
@@ -4,4 +4,3 @@ if __name__ == '__main__':
     PROJECT_PATH = configer.run_param("PROJECT_PATH")
     # 调用方法
     wordUtil.wordClearRepeat(PROJECT_PATH + "/res/org/resource", PROJECT_PATH + "dest/pic_word/", "result.txt")
-    # wordUtil.findFiles("../../res/org/resource")
