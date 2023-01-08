f9d3f5db292ec14f335dc403010ee644d60a453c
linguanghan <1354009873@qq.com>
2023-01-08 18:19:00 +0800

    update：代码优化

--- add_pinYin.py
+++ add_pinYin.py
@@ -1,7 +1,13 @@
 from script.utils.utils import utils
 from script.base.configer import configer
 
+
 def transformPinyin(fileRoot, errorFile):
+    '''
+    @param fileRoot: 需要转化的文件路径
+    @param errorFile: 输出的错误文件路径
+    @return:
+    '''
     errorFileData = ''
     for file in fileRoot:
         count = 0
@@ -9,21 +15,21 @@ def transformPinyin(fileRoot, errorFile):
             file_data = ''
             for line in f:
                 count = count + 1
-                if (len(line.split('\t')) != 5):
+                if len(line.split('\t')) != 5:
                     new_error = file + '下的第' + str(count) + '行tab键有误' + '\n'
                     errorFileData += new_error
                     new_line = line
                 else:
-                    if(count == 1):
+                    if count == 1:
                         line_content = line.split('\t')
                         line_content.insert(4, '词语拼音')
-                        new_line= '\t'.join(line_content)
-                    elif(count == 2):
+                        new_line = '\t'.join(line_content)
+                    elif count == 2:
                         line_content = line.split('\t')
                         line_content.insert(4, 'CiYuPinyin')
                         new_line = '\t'.join(line_content)
                     else:
-                        if(line[-2] == '1'):
+                        if line[-2] == '1':
                             new_line = line
                         else:
                             line_content = line.split('\t')
@@ -32,20 +38,18 @@ def transformPinyin(fileRoot, errorFile):
                 file_data += new_line
             with open(file, "w", encoding="utf-8") as fi:
                 fi.write(file_data)
-    with open(errorFile, "w", encoding="utf-8") as fe:
-        fe.write(errorFileData)
+    if errorFileData != '':
+        with open(errorFile, "w", encoding="utf-8") as fe:
+            fe.write(errorFileData)
+
 
 if __name__ == '__main__':
     PROJECT_PATH = configer.run_param("PROJECT_PATH")
     utils.resFileList = []
-    utils.findFiles(PROJECT_PATH + "小学语文/", r"ChnWriteZi_[0-9]*.txt$")
-    utils.findFiles(PROJECT_PATH + "小学语文/", r"CHNWord_[0-9]*.txt$")
-    errFile = PROJECT_PATH + "小学语文/error.txt"
-    res = utils.pinyin("你好")
-
+    utils.findFiles(PROJECT_PATH + "/res/org/小学语文/", r"ChnWriteZi_[0-9]*.txt$")
+    utils.findFiles(PROJECT_PATH + "/res/org/小学语文/", r"CHNWord_[0-9]*.txt$")
+    errFile = PROJECT_PATH + "/res/org/小学语文/error.txt"
 
     transformPinyin(utils.resFileList, errFile)
 
-
-
-        # print(res)
+    # print(res)
