1b96afbfa2735d0bd96bce69cddd3f22d49c967c
sengmei <huiliuliu@126.com>
2023-01-08 17:48:49 +0800

    添加拼音

---
+++ add_pinYin.py
@@ -0,0 +1,51 @@
+from script.utils.utils import utils
+from script.base.configer import configer
+
+def transformPinyin(fileRoot, errorFile):
+    errorFileData = ''
+    for file in fileRoot:
+        count = 0
+        with open(file, "r", encoding="utf-8") as f:
+            file_data = ''
+            for line in f:
+                count = count + 1
+                if (len(line.split('\t')) != 5):
+                    new_error = file + '下的第' + str(count) + '行tab键有误' + '\n'
+                    errorFileData += new_error
+                    new_line = line
+                else:
+                    if(count == 1):
+                        line_content = line.split('\t')
+                        line_content.insert(4, '词语拼音')
+                        new_line= '\t'.join(line_content)
+                    elif(count == 2):
+                        line_content = line.split('\t')
+                        line_content.insert(4, 'CiYuPinyin')
+                        new_line = '\t'.join(line_content)
+                    else:
+                        if(line[-2] == '1'):
+                            new_line = line
+                        else:
+                            line_content = line.split('\t')
+                            line_content.insert(4, utils.pinyin(line_content[3]))
+                            new_line = '\t'.join(line_content)
+                file_data += new_line
+            with open(file, "w", encoding="utf-8") as fi:
+                fi.write(file_data)
+    with open(errorFile, "w", encoding="utf-8") as fe:
+        fe.write(errorFileData)
+
+if __name__ == '__main__':
+    PROJECT_PATH = configer.run_param("PROJECT_PATH")
+    utils.resFileList = []
+    utils.findFiles(PROJECT_PATH + "小学语文/", r"ChnWriteZi_[0-9]*.txt$")
+    utils.findFiles(PROJECT_PATH + "小学语文/", r"CHNWord_[0-9]*.txt$")
+    errFile = PROJECT_PATH + "小学语文/error.txt"
+    res = utils.pinyin("你好")
+
+
+    transformPinyin(utils.resFileList, errFile)
+
+
+
+        # print(res)
