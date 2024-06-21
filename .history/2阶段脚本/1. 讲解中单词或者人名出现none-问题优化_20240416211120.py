0c0974e86f85b23a04ff860bd27d7dc13c9dd283
daizai6261 <15359284261@163.com>
2024-06-22 00:27:02 +0800

    update：gitignore

---
+++ .history/2阶段脚本/1. 讲解中单词或者人名出现none-问题优化_20240416211120.py
@@ -0,0 +1,13 @@
+# import platform
+# print(platform.architecture())
+
+import pandas as pd
+
+import sys
+sys.path.append('D:\Workship\Pelbs\Gen\project\2阶段脚本')
+import config
+
+root_df = pd.read_excel(config.book_id_path)
+
+print(root_df)
+
